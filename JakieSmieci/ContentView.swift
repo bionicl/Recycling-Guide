5fd6ec9d4c87de0ac0ea23827e9b4d0ab1cd3802
Bionicl <maciejmaj@me.com>
2020-06-03 19:09:50 +0200

    Initial Commit

---
+++ JakieSmieci/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JakieSmieci
+//
+//  Created by Maciej Maj on 03/06/2020.
+//  Copyright © 2020 Teal Fire. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
